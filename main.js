67657edd024819c63431212304b14a21f7d52c97
farrukh0913 <farrukh0913@gmail.com>
2024-02-14 18:47:23 +0500

    initial commit

---
+++ main.js
@@ -0,0 +1,43 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const url = require('url');
+
+let win;
+
+function createWindow() {
+    win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            nodeIntegration: true,
+            allowFileAccess: true
+        }
+    });
+
+    win.loadURL(
+        url.format({
+            pathname: path.join(__dirname, 'dist/my-electron-app/browser/index.html'),
+            protocol: 'file:',
+            slashes: true
+        })
+    );
+
+    win.on('closed', () => {
+        win = null;
+    });
+    win.webContents.openDevTools();
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (win === null) {
+        createWindow();
+    }
+});
